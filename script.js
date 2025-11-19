a56158278ba78d37798b09ca55c9edd652bd0612
montezzs <leandraoliveroliver@gmail.com>
2025-11-19 00:24:21 -0300

    update

--- script.js
+++ script.js
@@ -34,4 +34,31 @@ document.getElementById("btnTopo").onclick = function() {
     window.scrollTo({ top: 0, behavior: "smooth" });
 };
 
+document.addEventListener("click", function(e) {
+    // Cria o círculo
+    const ripple = document.createElement("span");
+    ripple.className = "ripple-effect";
+    
+    // Define posição
+    ripple.style.left = e.clientX + "px";
+    ripple.style.top = e.clientY + "px";
 
+    // Adiciona ao body
+    document.body.appendChild(ripple);
+
+    // Remove depois da animação
+    setTimeout(() => {
+        ripple.remove();
+    }, 600);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cursor = document.createElement("div");
+    cursor.classList.add("cursor-ball");
+    document.body.appendChild(cursor);
+
+    window.addEventListener("mousemove", (e) => {
+        cursor.style.left = e.clientX + "px";
+        cursor.style.top  = e.clientY + "px";
+    });
+});
